dc32e2585898e4f5204dc7665c58d4804da7078d
skiier33 <akruglick3@gmail.com>
2024-10-16 19:20:06 -0600

    merge rebased arug-main onto mast

---
+++ tvdataclass/plot_classes.py
@@ -0,0 +1,27 @@
+import plotly.graph_objects as go
+import pandas as pd
+from DataClass import HoldCandle
+
+import plotly.graph_objects as go
+import pandas as pd
+
+
+
+
+class BasePlot(HoldCandle):
+
+    def __init__(self, interval, n_bars):
+        self.interval=interval
+        self.n_bars=n_bars
+        self.plot_df()
+
+    def plot_df(self):
+
+        fig = go.Figure(data=[go.Candlestick(x=self.df.index,
+                        open=self.df['Open'], high=self.df['High'],
+                        low=self.df['Low'], close=self.df['Close'])])
+
+        fig.update_layout(title=self.name, xaxis_title='Date', yaxis_title='Price')
+
+
+        fig.show()
